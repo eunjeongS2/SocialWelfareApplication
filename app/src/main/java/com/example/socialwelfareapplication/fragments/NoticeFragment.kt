ef1053282add79c5a50482e5c96df618a38fc6e9
eunjeongS2 <dmswjd2340@gmail.com>
2020-01-04 16:36:23 +0900

    Add notice search

--- app/src/main/java/com/example/socialwelfareapplication/fragments/NoticeFragment.kt
+++ app/src/main/java/com/example/socialwelfareapplication/fragments/NoticeFragment.kt
@@ -5,6 +5,7 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.SearchView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.RecyclerView
@@ -12,10 +13,12 @@ import com.example.socialwelfareapplication.R
 import com.example.socialwelfareapplication.adapters.NoticeItemListAdapter
 import com.example.socialwelfareapplication.models.Notice
 import com.example.socialwelfareapplication.viewmodels.NoticeViewModel
+import com.jakewharton.rxbinding3.widget.queryTextChanges
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.disposables.CompositeDisposable
 import io.reactivex.rxkotlin.addTo
 import kotlinx.android.synthetic.main.fragment_notice.view.*
+import java.util.concurrent.TimeUnit
 
 
 class NoticeFragment : Fragment() {
@@ -23,6 +26,11 @@ class NoticeFragment : Fragment() {
     private lateinit var viewModel: NoticeViewModel
     private lateinit var adapter: NoticeItemListAdapter
     private val disposeBag = CompositeDisposable()
+    private val searchDisposeBag = CompositeDisposable()
+
+    companion object {
+        const val QUERY_TIMEOUT = 500L
+    }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
@@ -40,6 +48,9 @@ class NoticeFragment : Fragment() {
         viewModel.noticePublisher.observeOn(AndroidSchedulers.mainThread())
             .subscribe({ noticeList ->
                 setupItems(adapter, noticeList)
+
+                view?.noticeSearchView?.setSearch(noticeList)
+
 
             }, { e ->
                 Log.d(MonitoringFragment.TAG, "e : ", e)
@@ -70,6 +81,35 @@ class NoticeFragment : Fragment() {
         }
 
         return view
+    }
+
+    private fun SearchView.setSearch(noticeList: List<Notice>) {
+        searchDisposeBag.clear()
+        this.queryTextChanges().debounce(QUERY_TIMEOUT, TimeUnit.MILLISECONDS)
+            ?.observeOn(AndroidSchedulers.mainThread())
+            ?.subscribe({ search ->
+                if (search.isBlank()) {
+                    adapter.noticeList = noticeList
+                    adapter.notifyDataSetChanged()
+
+                    return@subscribe
+                }
+                val result = query(noticeList, search.toString())
+
+                adapter.noticeList = result
+                adapter.notifyDataSetChanged()
+
+            }, { e ->
+                e.printStackTrace()
+            })?.addTo(disposeBag)
+
+    }
+
+    private fun query(monitoringList: List<Notice>, search: String): List<Notice> {
+        return monitoringList.filter {
+            it.date.contains(search) || it.title.contains(search) || it.body.contains(search)
+
+        }
     }
 
     private fun setupItems(adapter: NoticeItemListAdapter, itemList: List<Notice>) {
